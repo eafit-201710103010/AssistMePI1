63dbd01f307abe0c8d83469368308afe8a0a535f
rvillegasm <rvillegasm@eafit.edu.co>
2018-09-17 14:01:49 -0500

    Started RFID controller

---
+++ sensorDrivers/RFIDSensor/rfid_controller.py
@@ -0,0 +1,6 @@
+""" Python based RFID controller for raspberry pi
+
+Python module used to control an RFID scanner connected to a raspberry pi.
+It reads the the serial number in a magnetic card and checks if that person is registered or not.
+"""
+
