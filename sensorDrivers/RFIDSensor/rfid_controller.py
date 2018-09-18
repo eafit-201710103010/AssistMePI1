f4b4d5d25224334ae86782ef8ee715ecaa30067c
rvillegasm <rvillegasm@eafit.edu.co>
2018-09-17 21:37:31 -0500

    Changes to rfid scanner

--- sensorDrivers/RFIDSensor/rfid_controller.py
+++ sensorDrivers/RFIDSensor/rfid_controller.py
@@ -4,3 +4,7 @@ Python module used to control an RFID scanner connected to a raspberry pi.
 It reads the the serial number in a magnetic card and checks if that person is registered or not.
 """
 
+import serial
+import re, sys, signal, os, time, datetime
+import RPi.GPIO as GPIO
+
