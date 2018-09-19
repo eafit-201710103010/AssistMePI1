a39a2ab211dcaad0d11e1d656ac9287be2eff4b1
lmvasquezg <lmvasquezg@eafit.edu.co>
2018-09-19 18:05:10 -0500

    RFID linker finished

--- linkers/rfid.js
+++ linkers/rfid.js
@@ -4,13 +4,35 @@
  * It uses the python-shell node module to do this.
  */
 
+
+
 function rfid_scan() {
 
   const python = require("python-shell");
   const path = require("path");
 
   const options = {
-    scriptPath : path.join(__dirname, '/../sensorDrivers/RFIDSensor')
+    scriptPath : path.join(__dirname, '/../sensorDrivers/RFIDSensor/')
   }
+
+  var serialID = new python("rfid_controller.py",options);
+
+  var nombre = document.getElementById("nombre").value;
+  var codigo = document.getElementById("codigo").value;
+  var docIdentidad = document.getElementById("docIdentidad").value;
+  var edad = document.getElementById("edad").value;
+  var sexoh = document.getElementById("sexoH").value;
+  var sexom = document.getElementById("sexoM").value;
+
+  const options2 = {
+    scriptPath : patn.join(_dirname,'/'),
+    args: [serialID,nombre,codigo,docIdentidad,edad,sexoh,sexom]
+  } 
+  
+  var auxiliar = new python("writeFile.py",options2);
+
+  sexom.end((err, code, message) => {
+    swal("Persona Añadida Exitosamente");
+  })
 
 }
