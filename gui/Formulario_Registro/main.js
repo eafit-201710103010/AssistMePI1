baf6343b868c0356941bbc664cb9218a11a80706
Isabela Muriel Roldan <imurielr@eafit.edu.co>
2018-09-21 00:13:56 +0000

    Pequeño cambie en el tamaño de la pantalla del formulario de registro

--- gui/Formulario_Registro/main.js
+++ gui/Formulario_Registro/main.js
@@ -2,7 +2,7 @@ const{app, BrowserWindow} = require('electron')
 
     function createWindow(){
         //Creación de la ventana del navegador
-        window = new BrowserWindow({width: 800, height: 600})
+        window = new BrowserWindow({width: 1500, height: 800})
         //Cargar archivo main.html de la app
         window.loadFile('main.html')
     }
