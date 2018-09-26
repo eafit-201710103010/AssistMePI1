cab00db3a251aad49fc82b5cc03df87a3316d5a7
Isabela <imurielr@eafit.edu.co>
2018-09-26 17:59:17 -0500

    Inicio de la pagina principal

---
+++ gui/main.js
@@ -0,0 +1,9 @@
+const{app, BrowserWindow} = require('electron')
+
+    function createWindow(){
+        //Creación de la ventana del navegador
+        window = new BrowserWindow({width: 1500, height: 700})
+        //Cargar archivo main.html de la app
+        window.loadFile('main.html')
+    }
+app.on('ready', createWindow)
