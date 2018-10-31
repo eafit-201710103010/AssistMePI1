0036443159478a36740bd29ec5db7da891e5e195
rvillegasm <rvillegasm@eafit.edu.co>
2018-10-31 15:37:00 -0500

    user log-in resource added

--- server/assistme_server.py
+++ server/assistme_server.py
@@ -13,7 +13,7 @@ from resources.add_event import AddEvent
 from resources.download_event import DownloadEvent
 from resources.manage_users import ManageUsers
 from resources.remove_event import RemoveEvent
-
+from resources.user_log_in import LogIn
 
 # Create the app and define it as a rest api.
 app = Flask(__name__)
@@ -25,6 +25,7 @@ api.add_resource(AddEvent, "/add_event")
 api.add_resource(DownloadEvent, "/download_event/<string:nombre_evento>")
 api.add_resource(ManageUsers, "/manage_users")
 api.add_resource(RemoveEvent, "/remove_event")
+api.add_resource(LogIn, "/log_in")
 
 
 # Main --> run the server
---
+++ server/resources/user_log_in.py
@@ -0,0 +1,43 @@
+"""All the logic for the user log-in process is located here."""
+
+# Import dependencies from flask_restful, as well as the database models necessary and the session to connect to it
+from flask_restful import Resource, reqparse
+from sqlalchemy import and_
+from resources.database.db_session import session
+from resources.database.models import Usuario
+# Also import the AESCipher from tools in order to encrypt and decrypt user passwords, as well as it's key
+from resources.tools.aes_encryption import AESCipher, secret_key
+
+# Create the parser for the requests and add all the expected arguments
+log_in_parser = reqparse.RequestParser()
+log_in_parser.add_argument("nombre")
+log_in_parser.add_argument("password")
+
+class LogIn(Resource):
+  """ Class used to manage all the logic for user log in """
+  # when a post request arrives, parse the arguments, look for that user in the database and say if it exists or not
+  # also returns a 200 HTTP status code indicating that the GET request was succesfull
+  # or a 500 HTTP status code indicating an internal server error if the GET request was not successful
+  def get(self):
+    # Parse Aguments
+    args = log_in_parser.parse_args()
+
+    # Create the chiper object and encypt the password
+    cipher = AESCipher(secret_key)
+    user_password = args["password"]
+    encrypted_password = cipher.encrypt(user_password)
+
+    usuario = session.query(Usuario).filter( and_(
+                                                  Usuario.nombre == args["nombre"],
+                                                  Usuario.password == encrypted_password
+                                                  ))
+    
+    info_usuario = {
+                    "nombre": usuario.nombre,
+                    "password": usuario.password,
+                    "permiso": usuario.permiso
+                   }
+
+    return info_usuario, 200
+
+    
